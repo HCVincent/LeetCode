76f9238ab31db488b513b07f373b81db730e29e7
HCVincent <lilacsweet@163.com>
2022-09-30 20:33:44 +0800

    upgrade

--- src/array/P1TwoSum.java
+++ src/array/P1TwoSum.java
@@ -1,9 +1,9 @@
-package p1;
+package array;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+public class P1TwoSum {
 	public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
