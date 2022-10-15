3d802a6f4074d68eb9d804e28ed0d2f8de80b705
HCVincent <lilacsweet@163.com>
2022-10-15 19:09:46 +0800

    LC - P4

--- src/array/P4MedianOfTwoSortedArrays.java
+++ src/array/P4MedianOfTwoSortedArrays.java
@@ -4,29 +4,27 @@ public class P4MedianOfTwoSortedArrays {
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int index1 = 0;
 		int index2 = 0;
-		int med1 = 0;
-		int med2 = 0;
+		int m1 = 0;
+		int m2 = 0;
 		for (int i = 0; i <= (nums1.length + nums2.length) / 2; i++) {
-			med1 = med2;
+			m1 = m2;
 			if (index1 == nums1.length) {
-				med2 = nums2[index2];
+				m2 = nums2[index2];
 				index2++;
 			} else if (index2 == nums2.length) {
-				med2 = nums1[index1];
+				m2 = nums1[index1];
 				index1++;
-			} else if (nums1[index1] < nums2[index2]) {
-				med2 = nums1[index1];
-				index1++;
-			} else {
-				med2 = nums2[index2];
+			} else if (nums1[index1] >= nums2[index2]) {
+				m2 = nums2[index2];
 				index2++;
+			} else {
+				m2 = nums1[index1];
+				index1++;
 			}
 		}
-
 		if ((nums1.length + nums2.length) % 2 == 0) {
-			return (float) (med1 + med2) / 2;
+			return (float) (m1 + m2) / 2;
 		}
-
-		return med2;
+		return m2;
 	}
 }
