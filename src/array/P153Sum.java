b18a8d1a39f430df92e520bf4a0d3f441c2b5e60
HCVincent <lilacsweet@163.com>
2022-10-17 10:25:02 +0800

    LC - P15

--- src/array/P153Sum.java
+++ src/array/P153Sum.java
@@ -6,29 +6,36 @@ import java.util.List;
 
 public class P153Sum {
 	public List<List<Integer>> threeSum(int[] nums) {
-		List<List<Integer>> result = new ArrayList<>();
-		if (nums.length < 3)
-			return result;
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		Arrays.sort(nums);
-		int i = 0;
-		while (i < nums.length - 2) {
-			if (nums[i] > 0)
-				break;
+		for (int i = 0; i < nums.length - 2; i++) {
 			int j = i + 1;
 			int k = nums.length - 1;
+			if (nums[i] > 0 || nums[k] < 0) {
+				break;
+			}
+			if (i > 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
 			while (j < k) {
 				int sum = nums[i] + nums[j] + nums[k];
-				if (sum == 0)
+				if (sum == 0) {
 					result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-				if (sum <= 0)
-					while (nums[j] == nums[++j] && j < k)
-						;
-				if (sum >= 0)
-					while (nums[k--] == nums[k] && j < k)
-						;
+					while (j < k && nums[j] == nums[j + 1]) {
+						j++;
+					}
+					while (j < k && nums[k] == nums[k - 1]) {
+						k--;
+					}
+					j++;
+					k--;
+				} else if (sum > 0) {
+					k--;
+				} else {
+					j++;
+				}
 			}
-			while (nums[i] == nums[++i] && i < nums.length - 2)
-				;
+
 		}
 		return result;
 	}
