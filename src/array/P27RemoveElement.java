0cd16a70771b24f76c72afb52f13b7a2d09a0862
HCVincent <lilacsweet@163.com>
2023-01-10 02:32:39 +0800

    p27 solved

---
+++ src/array/P27RemoveElement.java
@@ -0,0 +1,18 @@
+package array;
+
+public class P27RemoveElement {
+	public int removeElement(int[] nums, int val) {
+	    int i = 0;
+	    int n = nums.length;
+	    while (i < n) {
+	        if (nums[i] == val) {
+	            nums[i] = nums[n - 1];
+	            // reduce array size by one
+	            n--;
+	        } else {
+	            i++;
+	        }
+	    }
+	    return n;
+	}
+}
